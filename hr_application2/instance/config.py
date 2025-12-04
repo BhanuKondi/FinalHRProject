c3c8fc692ce20368f188da5d31afb66c2e64d417
BhanuKondi <bhanu@atikes.com>
2025-12-04 18:32:46 +0530

    Update config.py

--- hr_application2/instance/config.py
+++ hr_application2/instance/config.py
@@ -1,6 +1,6 @@
 # Copy your real DB credentials here
 MYSQL_HOST = "localhost"
 MYSQL_USER = "hr_user"
-MYSQL_PASSWORD = "YourPassword123!"
+MYSQL_PASSWORD = "YourPassword"
 MYSQL_DATABASE = "hr_app"
 SECRET_KEY = "xyz"
